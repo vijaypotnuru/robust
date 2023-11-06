e8becc2790e066d6e3225103a3567c2f31cf62e5
Durga Potnuru <vijaypotnuru123@gmail.com>
2023-11-06 19:47:32 +0530

    topbar ,leftsidebar and some route added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import { Routes, Route } from "react-router-dom";
 
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 import "./globals.css";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
@@ -22,6 +32,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSideBar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/Topbar";
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <div className='w-full md:flex'></div>
-  )
-}
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSideBar />
+      <section className="flex flex-1 h-full">{<Outlet />}</section>
+      <Bottombar />
+    </div>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
